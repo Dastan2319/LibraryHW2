17e545a99cde8c3822128d4b099322fae4a8e6d1
dastan2319 <dastan-4@mail.ru>
2019-10-22 19:38:54 +0600

    ready?

---
+++ WebApplication1/Controllers/GanreController.cs
@@ -0,0 +1,37 @@
+﻿using BLL.DTO;
+using BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class GanreController : Controller
+    {
+        IGanreService ganreService;
+
+        public GanreController(IGanreService serv)
+        {
+            ganreService = serv;
+        }
+        
+        public ActionResult Create()
+        {
+            GanreDTO ganre = new GanreDTO();
+            return View(ganre);
+        }
+        
+        [HttpPost]
+        public ActionResult Create(GanreDTO collection)
+        {
+            var bookDto = new GanreDTO { FirstName=collection.FirstName };
+            ganreService.MakeGanre(bookDto);
+            return RedirectToActionPermanent("EditOrCreate", "Main");
+        }
+
+
+       
+    }
+}
